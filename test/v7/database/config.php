b9d101a5eaef5f70776600450a5802d673fd773c
nrb2002 <tshibasubaron@yahoo.fr>
2023-02-03 19:23:35 +0100

    New update

---
+++ test/v7/database/config.php
@@ -0,0 +1,33 @@
+<?php
+
+/*This file manages the connection to the server. 
+It can be imported in the entire project where the database connection will be needed
+To import it, just include the following in the concerned file: 
+
+<?php
+    include('database/config.php');
+?>
+
+*/
+
+    //Server connection variables
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "members";
+
+    //Set connection
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //test connection
+        //echo "Connected successfully"; 
+        
+        //If connection failed, throw error message 
+    }catch(PDOException $error) {
+        $message = "Connection failed: " . $error->getMessage();
+    }
+
+?>
